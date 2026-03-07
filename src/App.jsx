e96688f29adac2f681fffe9784710d4a2eb890f5
sanjeev9090 <146428803+sanjeev9090@users.noreply.github.com>
2026-03-07 17:26:07 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<AboutUs />} />
+        
         {/* 
         <Route path="/services" element={<Services />} />
         <Route path="/industries" element={<Industries />} />
