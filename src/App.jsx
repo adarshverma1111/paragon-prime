db470464eb816bea958a0665ad1ef3a6193a7300
Adarsh Verma <162157980+adarshverma1111@users.noreply.github.com>
2026-03-08 12:30:19 +0530

    industries added

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ import Contact from "./components/Home/ContactPage";
 import ERPSolutions from "./pages/ERPSolutions";
 import CRMSolutions from "./pages/CRMSolutions";
 import AboutUs from "./components/Aboutus/AboutUs";
-import ServicesPage from "./components/Home/ServicesPage";
+import ServicesPage from "./components/Services/ServicesPage";
+import IndustriesPage from "./components/industries/IndustriesPage";
 
 function App() {
   return (
